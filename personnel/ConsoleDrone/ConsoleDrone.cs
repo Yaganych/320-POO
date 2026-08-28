787fb871aa5eebe8e91e5ce79c04986247b55711
Yaganych <babayagakiril@gmail.com>
2026-08-28 16:01:00 +0200

    doc(git journal): create a Git journal
    [5][done]

--- personnel/ConsoleDrone/ConsoleDrone.cs
+++ personnel/ConsoleDrone/ConsoleDrone.cs
@@ -15,9 +15,9 @@ namespace ConsoleApp1
 
             while (drone1.battery > 0)
             {
-                drone1.ConsoleToShow();
-                drone1.StatusChange();
-                drone1.ToDieMessage();
+                drone1.ConsoleToShow(drone1.posX, drone1.POS_Y, drone1.TIME_TO_WAIT);
+                drone1.StatusChange(ref drone1.posX, ref drone1.battery);
+                drone1.ToDieMessage(drone1.posX, drone1.POS_Y, drone1.battery);
             }
             Console.ReadKey();
         }
@@ -25,9 +25,9 @@ namespace ConsoleApp1
         public class Drone
         {
             public int battery { get; set; } = 50 ;
-            public int posXfirstDrone { get; set; } = 0 ;
+            public int posX { get; set; } = 0 ;
             public string color { get; set; } = "Red" ;
-            public int POS_Y_FIRST_DRONE { get; set; } = 0 ;
+            public int POS_Y { get; set; } = 0 ;
             public int TIME_TO_WAIT { get; set; } = 100 ;
 
 
@@ -40,13 +40,13 @@ namespace ConsoleApp1
                 Console.Clear();
             }
 
-            static void StatusChange(ref int posX, ref int battery)
+            public void StatusChange(ref int posX, ref int battery)
             {
                 posX += 1;
                 battery -= 2;
             }
 
-            static void ToDieMessage(int posX, int posY, int battery)
+            public void ToDieMessage(int posX, int posY, int battery)
             {
                 Console.SetCursorPosition(posX, posY);
                 if (battery <= 0) Console.WriteLine("____");
