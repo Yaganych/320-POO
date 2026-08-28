86e00581a7baf2b6e9d49966ba9c676cd742eb71
Yaganych <babayagakiril@gmail.com>
2026-08-28 12:15:29 +0200

    fix(consoleDrone): create a class Drone

--- personnel/ConsoleDrone/ConsoleDrone.cs
+++ personnel/ConsoleDrone/ConsoleDrone.cs
@@ -11,33 +11,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int battery = 50;
-            int posX = 0;
-            int POS_Y = 0;
-            int TIME_TO_WAIT = 300;
+            var drone1 = new Drone();
 
-            while (battery > 0)
+            while (drone1.battery > 0)
             {
-                ConsoleToShow(posX, POS_Y, TIME_TO_WAIT);
-                StatusChange(ref posX, ref battery);
+                drone1.ConsoleToShow();
+                drone1.StatusChange();
+                drone1.ToDieMessage();
             }
-
-            Console.WriteLine("Il est mort");
             Console.ReadKey();
         }
 
-        static void StatusChange (ref int posX, ref int battery)
+        public class Drone
         {
-            posX += 2;
-            battery -= 2;
-        }
+            public int battery { get; set; } = 50 ;
+            public int posXfirstDrone { get; set; } = 0 ;
+            public string color { get; set; } = "Red" ;
+            public int POS_Y_FIRST_DRONE { get; set; } = 0 ;
+            public int TIME_TO_WAIT { get; set; } = 100 ;
 
-        static void ConsoleToShow(int posX, int posY, int timeToWait)
-        {
-            Console.SetCursorPosition(posX, posY);
-            Console.WriteLine("x-0-x");
-            Thread.Sleep(timeToWait);
-            Console.Clear();
+
+            public void ConsoleToShow(int posX, int posY, int timeToWait)
+            {
+                Console.SetCursorPosition(posX, posY);
+                Console.ForegroundColor = ConsoleColor.color;
+                Console.WriteLine("x-0-x");
+                Thread.Sleep(timeToWait);
+                Console.Clear();
+            }
+
+            static void StatusChange(ref int posX, ref int battery)
+            {
+                posX += 1;
+                battery -= 2;
+            }
+
+            static void ToDieMessage(int posX, int posY, int battery)
+            {
+                Console.SetCursorPosition(posX, posY);
+                if (battery <= 0) Console.WriteLine("____");
+                Console.SetCursorPosition(posX + 4, posY);
+                if (battery <= 0) Console.WriteLine("Il est mort");
+            }
         }
     }
 }
