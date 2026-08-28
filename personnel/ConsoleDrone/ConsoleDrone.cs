79e0f09eeb1e7d09a657d4bd88123e563b34ae23
Yaganych <babayagakiril@gmail.com>
2026-08-28 11:29:25 +0200

    feat(consoleDrone): create a program for exercice console droen

---
+++ personnel/ConsoleDrone/ConsoleDrone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int battery = 50;
+            int posX = 0;
+            const int POS_Y = 0;
+            const int TIME_TO_WAIT = 300;
+
+            Console.Clear();
+            Console.WriteLine("x-0-x");
+            Thread.Sleep(TIME_TO_WAIT);
+
+            while (battery > 0)
+            {
+                posX+= 2;
+                battery-=2;
+                Console.Clear();
+                Console.SetCursorPosition(posX, POS_Y);
+                Console.WriteLine("x-0-x");
+                Thread.Sleep(TIME_TO_WAIT);
+            }
+
+            Console.WriteLine("Il est mort");
+            Console.ReadKey();
+        }
+    }
+}
