b3dda07cb0b07fa97f1e81067cde53b7b5ddbf9c
Yaganych <babayagakiril@gmail.com>
2026-08-28 11:44:17 +0200

    fix(consoleDroen): add to methode to show and to change status

--- personnel/ConsoleDrone/ConsoleDrone.cs
+++ personnel/ConsoleDrone/ConsoleDrone.cs
@@ -13,25 +13,31 @@ namespace ConsoleApp1
         {
             int battery = 50;
             int posX = 0;
-            const int POS_Y = 0;
-            const int TIME_TO_WAIT = 300;
-
-            Console.Clear();
-            Console.WriteLine("x-0-x");
-            Thread.Sleep(TIME_TO_WAIT);
+            int POS_Y = 0;
+            int TIME_TO_WAIT = 300;
 
             while (battery > 0)
             {
-                posX+= 2;
-                battery-=2;
-                Console.Clear();
-                Console.SetCursorPosition(posX, POS_Y);
-                Console.WriteLine("x-0-x");
-                Thread.Sleep(TIME_TO_WAIT);
+                ConsoleToShow(posX, POS_Y, TIME_TO_WAIT);
+                StatusChange(ref posX, ref battery);
             }
 
             Console.WriteLine("Il est mort");
             Console.ReadKey();
+        }
+
+        static void StatusChange (ref int posX, ref int battery)
+        {
+            posX += 2;
+            battery -= 2;
+        }
+
+        static void ConsoleToShow(int posX, int posY, int timeToWait)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.WriteLine("x-0-x");
+            Thread.Sleep(timeToWait);
+            Console.Clear();
         }
     }
 }
